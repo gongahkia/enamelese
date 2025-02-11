85fd7e5f80c9376a26dd308ba56b6522f1be3fd8
gongahkia <angryapplegravy@gmail.com>
2025-02-11 23:57:32 +0800

    feat(app): error edge cases can eaty my ass

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,8 +1,10 @@
 module Interpreter (interpret) where
 
 import Parser (Expr(..))
+import Lexer (Token(..))
 import qualified Data.Map as M
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 type Environment = M.Map T.Text Value
 
@@ -12,6 +14,7 @@ data Value
   | VBoolean Bool
   | VList [Value]
   | VDict (M.Map T.Text Value)
+  | VFunction [T.Text] [Expr] Environment
   deriving (Show)
 
 interpret :: Expr -> IO ()
@@ -34,17 +37,23 @@ evalExpr (If cond thenStmts elseStmts) env = do
     VBoolean False -> foldl (\e s -> e >>= evalExpr s) (return env) elseStmts
     _ -> error "Condition must be a boolean"
 evalExpr (Loop cond body) env = evalLoop cond body env
-evalExpr (FuncDef name params body) env = return $ M.insert name (VFunction params body) env
+evalExpr (FuncDef name params body) env = return $ M.insert name (VFunction params body env) env
 evalExpr (FuncCall name args) env = do
   case M.lookup name env of
-    Just (VFunction params body) -> do
+    Just (VFunction params body closureEnv) -> do
       argValues <- mapM (`evalLiteral` env) args
-      let localEnv = M.fromList (zip params argValues) `M.union` env
+      let localEnv = M.fromList (zip params argValues) `M.union` closureEnv
       foldl (\e s -> e >>= evalExpr s) (return localEnv) body
     _ -> error $ "Undefined function: " ++ T.unpack name
-evalExpr expr env = do
+evalExpr (Print expr) env = do
   value <- evalLiteral expr env
   print value
+  return env
+evalExpr (Input name) env = do
+  input <- TIO.getLine
+  return $ M.insert name (VString input) env
+evalExpr expr env = do
+  value <- evalLiteral expr env
   return env
 
 evalLiteral :: Expr -> Environment -> IO Value
@@ -55,6 +64,17 @@ evalLiteral (Literal (TIdentifier name)) env =
   case M.lookup name env of
     Just value -> return value
     Nothing -> error $ "Undefined variable: " ++ T.unpack name
+evalLiteral (List exprs) env = do
+  values <- mapM (`evalLiteral` env) exprs
+  return $ VList values
+evalLiteral (Dict pairs) env = do
+  evaledPairs <- mapM (\(k, v) -> do
+    key <- evalLiteral k env
+    value <- evalLiteral v env
+    case key of
+      VString s -> return (s, value)
+      _ -> error "Dictionary keys must be strings") pairs
+  return $ VDict (M.fromList evaledPairs)
 evalLiteral expr _ = error $ "Invalid literal: " ++ show expr
 
 evalLoop :: Expr -> [Expr] -> Environment -> IO Environment
