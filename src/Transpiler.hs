e1eb6340dba85d7a8af729a4c6c9c13720d1999e
gongahkia <angryapplegravy@gmail.com>
2025-02-11 23:57:51 +0800

    feat(app): at least i can shortenn this

--- src/Transpiler.hs
+++ src/Transpiler.hs
@@ -22,16 +22,4 @@ transpileStmt (FuncDef name params body) =
   "def " ++ T.unpack name ++ "(" ++ T.unpack (T.intercalate ", " params) ++ "):\n" ++
   indent (concatMap transpileStmt body)
 transpileStmt (FuncCall name args) =
-  T.unpack name ++ "(" ++ intercalate ", " (map transpileExpr args) ++ ")"
-transpileStmt expr = transpileExpr expr
-
-transpileExpr :: Expr -> String
-transpileExpr (Literal (TString s)) = show (T.unpack s)
-transpileExpr (Literal (TNumber n)) = show n
-transpileExpr (Literal (TBoolean True)) = "True"
-transpileExpr (Literal (TBoolean False)) = "False"
-transpileExpr (Literal (TIdentifier name)) = T.unpack name
-transpileExpr expr = error $ "Unsupported expression: " ++ show expr
-
-indent :: String -> String
-indent = unlines . map ("    " ++) . lines
+  T.unpack name ++ "(" ++ intercalate ", " (map transpileEx
